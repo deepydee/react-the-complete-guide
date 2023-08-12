8966d97e487ee38deed5067d2f46a665c82c1da6
Vitaliy Semenov <synthex@bk.ru>
2023-08-12 23:02:55 +0600

    Time to Practice: Working with Events & State

--- code/01-starting-setup/src/components/Expensees/Expenses.jsx
+++ code/01-starting-setup/src/components/Expensees/Expenses.jsx
@@ -1,22 +1,40 @@
-import "./Expenses.css";
+import {useState} from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
   const expenses = [];
 
   props.items.forEach((expense) =>
-    expenses.push(
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    )
-  );
+  expenses.push(
+    <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    />
+  )
+);
 
-  return <Card className="expenses">{expenses}</Card>;
+  const filterChangeHandler = (year) => {
+    setFilteredYear(year);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expenses}
+      </Card>
+    </div>
+  );
 };
 
 export default Expenses;
