26650346822685d569af539c52d0cb48303535e1
Vitaliy Semenov <synthex@bk.ru>
2023-08-13 18:35:48 +0600

    fix: remove unnecessary logic

--- code/01-starting-setup/src/components/Expensees/ExpenseItem.jsx
+++ code/01-starting-setup/src/components/Expensees/ExpenseItem.jsx
@@ -1,24 +1,14 @@
-import {useState} from "react";
-
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  console.log("ExpenseItem is evaluated by React");
-
-  const clicHandler = () => {
-    setTitle("Updated");
-    console.log(title);
-  };
-
   return (
     <li>
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-          <h2>{title}</h2>
+          <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
       </Card>
